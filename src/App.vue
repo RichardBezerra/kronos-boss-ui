dbbda9e1dfe75c1cd6257e5b7ffdc12059b62cca
Richard Bezerra <richardbezerra@gmail.com>
2018-11-14 22:24:17 +0000

    Adding menu items

--- src/App.vue
+++ src/App.vue
@@ -5,29 +5,42 @@
       </md-app-toolbar>
 
       <md-app-drawer md-permanent="full">
-        <md-toolbar class="md-transparent" md-elevation="0">
-          Menu
-        </md-toolbar>
+        <md-toolbar class="md-transparent" md-elevation="0"></md-toolbar>
 
         <md-list>
           <md-list-item>
             <md-icon>move_to_inbox</md-icon>
-            <span class="md-list-item-text">Item 1</span>
+            <span class="md-list-item-text">Dashboards</span>
           </md-list-item>
 
           <md-list-item>
             <md-icon>send</md-icon>
-            <span class="md-list-item-text">Item 2</span>
+            <span class="md-list-item-text">Departamentos</span>
           </md-list-item>
 
           <md-list-item>
             <md-icon>delete</md-icon>
-            <span class="md-list-item-text">Items 3</span>
+            <span class="md-list-item-text">Turnos</span>
           </md-list-item>
 
           <md-list-item>
             <md-icon>error</md-icon>
-            <span class="md-list-item-text">Item 4</span>
+            <span class="md-list-item-text">Colaboradores</span>
+          </md-list-item>
+
+           <md-list-item>
+            <md-icon>error</md-icon>
+            <span class="md-list-item-text">Relatórios</span>
+          </md-list-item>
+
+           <md-list-item>
+            <md-icon>error</md-icon>
+            <span class="md-list-item-text">Afastamentos</span>
+          </md-list-item>
+
+           <md-list-item>
+            <md-icon>error</md-icon>
+            <span class="md-list-item-text">Tablets</span>
           </md-list-item>
         </md-list>
       </md-app-drawer>
